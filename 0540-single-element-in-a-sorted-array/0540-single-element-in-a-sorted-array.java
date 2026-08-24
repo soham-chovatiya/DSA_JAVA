3dc7a62f5b0f5ebbfb3dbf33080394ee2b22367a
Soham Chovatiya <sohamchovatiya24@gmail.com>
2026-08-24 18:11:10 +0530

    Time: 15 ms (5%), Space: 53.8 MB (5.04%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> value : map.entrySet()) {
+            if (value.getValue() == 1) {
+                return value.getKey();
+        }
+}
+        return -1;
+    }
+}
